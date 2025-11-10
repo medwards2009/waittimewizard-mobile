be271df6cb2252a13f0ad77c2426a2239047766d
Michael Edwards <michaeledwards2009@gmail.com>
2025-11-09 21:33:01 -0500

    rough style of tabs nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Platform } from 'react-native';
 
 const TabsLayout = () => {
   return (
@@ -7,7 +8,27 @@ const TabsLayout = () => {
       screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: 'rgb(40, 40, 40)',
-        tabBarStyle: { backgroundColor: '#f8f8f8' },
+        tabBarInactiveTintColor: 'rgb(140, 140, 140)',
+        tabBarStyle: {
+          backgroundColor: 'rgba(255, 255, 255, 0.7)',
+          position: 'absolute',
+          bottom: Platform.OS === 'ios' ? 25 : 16,
+          elevation: 4,
+          marginLeft: '5%',
+          width: '90%',
+          borderRadius: 15,
+          height: 60,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 4,
+          },
+          shadowOpacity: 0.2,
+          shadowRadius: 12,
+          paddingBottom: 0,
+          borderWidth: 1,
+          borderColor: 'rgba(255, 255, 255, 0.7)',
+        },
       }}
     >
       <Tabs.Screen name="index" options={{ title: 'Magic Kingdom' }} />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,33 @@
-import { Text } from 'react-native';
-
+import { Text, View, StyleSheet } from 'react-native';
 import ThemedImageBackground from '@/components/styled-image-background';
 
 const MagicKingdom = () => {
   return (
+    // <View style={styles.container}>
     <ThemedImageBackground source={require('@/assets/images/mk.jpg')}>
+      {/* <View style={styles.content}> */}
       <Text>Welcome to the Magic Kingdom!</Text>
+      {/* </View> */}
     </ThemedImageBackground>
+    // </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 100, // Add padding to account for floating tabs
+  },
+});
 
 export default MagicKingdom;
